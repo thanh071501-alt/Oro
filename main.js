92c8bc80fb0f99bacc1182eb62221b7f4260ff53
thanh071501-alt <thanh071501@gmail.com>
2025-08-04 13:17:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ const ORO_AMOUNT = getRandomAmount();
 const LIQ_ORO = getRandomAmount();
 
 const delay = async (ms) => {
-    console.log(`⏳ Đang chờ ${ms / 1000} giây...`);
+   process.stdout.write(`\r⏳ Đang chờ ${i} giây... `);
     await new Promise(res => setTimeout(res, ms));
     console.log("✅ Hoàn thành chờ");
 };
